ed82b3bc231724f3009ea1403848cb86a12edd37
Vlada B <vlada.bessikalo@gmail.com>
2024-11-30 18:36:52 +0100

    complete prep-ex w2

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -1,10 +1,99 @@
-const express = require('express')
+const express = require('express');
 const app = express();
- 
+const fs = require('fs');
+const path = require('path');
 
-// YOUR CODE GOES IN HERE
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+app.use(express.json());
+
+const getFileName = (title) => {
+    return path.join(__dirname, `${title.replace(/\s+/g, '_')}.txt`);
+};
+
+const fileExists = (fileName) => {
+    return fs.existsSync(fileName);
+};
+
+const handleFileOperationError = (err, res) => {
+    console.error(err);
+    res.status(500).send('Internal server error.');
+};
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.post('/blogs', (req, res) => {
+    const { title, content } = req.body;
+    if (!title || !content) {
+        return res
+            .status(400)
+            .send('Bad Request. Both title and content are required!');
+    }
+
+    const fileName = getFileName(title);
+
+    try {
+        fs.writeFileSync(fileName, content);
+        res.end('ok');
+    } catch (err) {
+        handleFileOperationError(err, res);
+    }
+});
+
+app.put('/posts/:title', (req, res) => {
+    const { title } = req.params;
+    const { content } = req.body;
+
+    if (!content) {
+        return res.status(400).send('Bad Request. Content is required!');
+    }
+
+    const fileName = getFileName(title);
+
+    if (fileExists(fileName)) {
+        try {
+            fs.writeFileSync(fileName, content);
+            res.end('ok');
+        } catch (err) {
+            handleFileOperationError(err, res);
+        }
+    } else {
+        res.status(404).send('This post does not exist!');
+    }
+});
+
+app.delete('/blogs/:title', (req, res) => {
+    const { title } = req.params;
+    const fileName = getFileName(title);
+
+    if (fileExists(fileName)) {
+        try {
+            fs.unlinkSync(fileName);
+            res.end('ok');
+        } catch (err) {
+            handleFileOperationError(err, res);
+        }
+    } else {
+        res.status(404).send('This post does not exist!');
+    }
+});
+
+app.get('/blogs/:title', (req, res) => {
+    const { title } = req.params;
+    const fileName = getFileName(title);
+
+    if (fileExists(fileName)) {
+        try {
+            const post = fs.readFileSync(fileName, 'utf8');
+            res.status(200).send(post);
+        } catch (err) {
+            handleFileOperationError(err, res);
+        }
+    } else {
+        res.status(404).send('This post does not exist!');
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
