fff4d4b068e60e2dcc298777956362bde8af10fb
Vlada B <vlada.bessikalo@gmail.com>
2024-12-06 19:15:46 +0100

    complete prep-ex week3

--- week3/prep-exercise/server/app.js
+++ week3/prep-exercise/server/app.js
@@ -1,15 +1,27 @@
 import express from 'express';
-// TODO Use below import statement for importing middlewares from users.js for your routes
-// TODO import { ....... } from "./users.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { register, login, getProfile, logout } from './users.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 let app = express();
 
 app.use(express.json());
-// TODO: Create routes here, e.g. app.post("/register", .......)
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+app.post('/auth/register', register);
+app.post('/auth/login', login);
+app.get('/auth/profile', getProfile);
+app.post('/auth/logout', logout);
 
 // Serve the front-end application from the `client` folder
-app.use(express.static('client'));
+app.use(express.static(path.join(__dirname, '../client')));
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
